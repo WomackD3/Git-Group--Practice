ec1be1a19b96ff848a74cd7957c688a4a5ed7094
Gilbert Polanco <gilbertpolanco@GilbertancosMBP.lan>
2022-07-27 02:26:30 -0400

    added .env

--- Backend/server.js
+++ Backend/server.js
@@ -1,8 +1,15 @@
 const express = require("express");
+const dotenv = require("dotenv");
+dotenv.config()
+const dbConnect = require("./config/db/dbConnect");
+
 
 //server
 const app = express()
 
+//dbConnect
+dbConnect();
+console.log(process.env);
 const PORT = process.env.PORT || 3000;
 
 app.listen( PORT,console.log(`this bitch is up on ${PORT} YA HEARD!!`) )
