5bbaae8cd30c080549da6ddfbe9cf85e7e7ff9bd
Gilbert Polanco <gilbertpolanco@GilbertancosMBP.lan>
2022-07-29 00:04:59 -0400

    userschema setup

--- model/user/User.js
+++ model/user/User.js
@@ -1,17 +1,124 @@
 import mongoose from "mongoose";
-const Schema = mongoose.Schema;
 
-let House = new Schema({
-  _id: { type: String },
-  name: { type: String },
-  mascot: { type: String },
-  headOfHouse: { type: String },
-  houseGhost: { type: String },
-  founder: { type: String },
-  school: { type: String },
-  members: [{ type: Schema.Types.ObjectId, ref: "characters" }],
-  values: [{ type: String }],
-  colors: [{ type: String }],
-});
+//create schema
+const userSchema = new mongoose.Schema(
+  {
+    firstName: {
+      required: [true, "First name is required"],
+      type: String,
+    },
+    lastName: {
+      required: [true, "Last name is required"],
+      type: String,
+    },
+    profilePhoto: {
+      type: String,
+      default:
+        "https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_960_720.png",
+    },
+    email: {
+      type: String,
+      required: [true, "Email is required"],
+    },
+    bio: {
+      type: String,
+    },
+    password: {
+      type: String,
+      required: [true, "Hei buddy Password is required"],
+    },
+    postCount: {
+      type: Number,
+      default: 0,
+    },
+    isBlocked: {
+      type: Boolean,
+      default: false,
+    },
+    isAdmin: {
+      type: Boolean,
+      default: false,
+    },
+    role: {
+      type: String,
+      enum: ["Admin", "Guest", "Blogger"],
+    },
+    isFollowing: {
+      type: Boolean,
+      default: false,
+    },
+    isUnFollowing: {
+      type: Boolean,
+      default: false,
+    },
+    isAccountVerified: { type: Boolean, default: false },
+    accountVerificationToken: String,
+    accountVerificationTokenExpires: Date,
 
-export default mongoose.model("houses", House);
+    viewedBy: {
+      type: [
+        {
+          type: mongoose.Schema.Types.ObjectId,
+          ref: "User",
+        },
+      ],
+    },
+
+    followers: {
+      type: [
+        {
+          type: mongoose.Schema.Types.ObjectId,
+          ref: "User",
+        },
+      ],
+    },
+    following: {
+      type: [
+        {
+          type: mongoose.Schema.Types.ObjectId,
+          ref: "User",
+        },
+      ],
+    },
+    passwordChangeAt: Date,
+    passwordResetToken: String,
+    passwordResetExpires: Date,
+
+    active: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  {
+    toJSON: {
+      virtuals: true,
+    },
+    toObject: {
+      virtuals: true,
+    },
+    timestamps: true,
+  }
+);
+
+export default mongoose.model('userSchema', User)
+
+
+
+//referance schema 
+// const Schema = mongoose.Schema;
+
+// let House = new Schema({
+//   _id: { type: String },
+//   name: { type: String },
+//   mascot: { type: String },
+//   headOfHouse: { type: String },
+//   houseGhost: { type: String },
+//   founder: { type: String },
+//   school: { type: String },
+//   members: [{ type: Schema.Types.ObjectId, ref: "characters" }],
+//   values: [{ type: String }],
+//   colors: [{ type: String }],
+// });
+
+// export default mongoose.model("houses", House);
+
