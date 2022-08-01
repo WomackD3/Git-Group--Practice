0e47bc8f96b388543432f17f228da20e1a6d27c5
gil-p24 <gilbertpolanco42@gmail.com>
2022-08-01 02:18:14 -0400

    categoryScehma

--- model/category.js
+++ model/category.js
@@ -1,13 +1,19 @@
 import mongoose from "mongoose";
 
-const create = new mongoose.Schema({
-  user: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: "User",
-    required: true,
+const categorySchema = new mongoose.Schema(
+  {
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    title: {
+      type: String,
+      required: true,
+    },
+
   },
-  title: {
-    type: String,
-    required: true,
-  },
-} )
+  { timestamps: true }
+);
+
+export default mongoose.model('Category', categorySchema) 
