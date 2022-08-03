d3cee0e69b7f3f770f974c6521774e72e337a2d4
Womack <D_wom3@yahoo.com>
2022-08-03 17:41:17 -0400

    added seeding

--- config/db/dbConnect.js
+++ config/db/dbConnect.js
@@ -2,18 +2,28 @@ import mongoose from "mongoose";
 
 const MONGODB_URL = process.env.MONGODB_URL || "mongodb://127.0.0.1:27017/cyber-api"
 
+let mongooseConfig = {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+};
+
+let connectionString = "mongodb://127.0.0.1:27017/cyber-api"
+let db = mongoose.connection
+
+mongoose.connect(connectionString, mongooseConfig);
+  
 mongoose.set("returnOriginal", false);
 
 mongoose.connect(MONGODB_URL).catch((err) => {
   console.log(err);
 })
 
-mongoose.connection.on("disconnected", () => {
-  console.log('MongoDB disconnected');
-})
-
 mongoose.connection.on("error", (err) => {
   console.log(`MongoDB connection error: ${err}`);
 });
 
-export default mongoose.connection;
+mongoose.connection.on("disconnected", () => {
+  console.log('MongoDB disconnected');
+})
+
+export default db;
