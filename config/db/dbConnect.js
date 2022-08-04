d3d42772922741cac2e8d858bb8b85c2eccc219b
gil-p24 <gilbertpolanco42@gmail.com>
2022-08-04 03:54:27 -0400

    dbcoonect error

--- config/db/dbConnect.js
+++ config/db/dbConnect.js
@@ -10,7 +10,7 @@ let mongooseConfig = {
 let connectionString = "mongodb://127.0.0.1:27017/cyber-api"
 let db = mongoose.connection
 
-mongoose.connect(connectionString, mongooseConfig);
+// mongoose.connect(connectionString, mongooseConfig);
   
 mongoose.set("returnOriginal", false);
 
